41027436cbbd02888d13c84a3bb766625cb463e2
Taylor McKinney <2374670+taylormck@users.noreply.github.com>
2024-08-13 18:20:12 +0900

    feat: implement word

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,25 @@ use std::env;
 use std::io;
 use std::process;
 
+const ALPHANUMERIC_CHARACTERS: [char; 63] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4',
+    '5', '6', '7', '8', '9', '_',
+];
+
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
         input_line.contains(pattern)
-    } else if pattern.chars().count() == 2 && pattern.starts_with("\\d") {
-        input_line.parse::<u32>().is_ok()
+    } else if pattern.chars().count() == 2 {
+        if pattern.starts_with("\\d") {
+            input_line.parse::<u32>().is_ok()
+        } else if pattern.starts_with("\\w") {
+            let char = input_line.chars().next().unwrap();
+            ALPHANUMERIC_CHARACTERS.contains(&char)
+        } else {
+            panic!("Unhandled pattern: {}", pattern)
+        }
     } else {
         panic!("Unhandled pattern: {}", pattern)
     }
