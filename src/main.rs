568a810552379c242b2ad588bceceb9cdd430a33
Taylor McKinney <2374670+taylormck@users.noreply.github.com>
2024-08-18 18:27:29 +0900

    fix: match escaped backslashes

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,14 @@ static VALID_CHARACTERS: phf::Set<char> = phf_set! {
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4',
-    '5', '6', '7', '8', '9', '_', '-', ' '
+    '5', '6', '7', '8', '9', '_', '-', ' ',
 };
 
 fn match_escape_pattern(input_characters: &mut core::str::Chars, escape_pattern: &char) -> bool {
     match escape_pattern {
-        'd' => input_characters.any(|char| NUMERIC_CHARACTERS.contains(&char)),
-        'w' => input_characters.any(|char| ALPHANUMERIC_CHARACTERS.contains(&char)),
+        'd' => NUMERIC_CHARACTERS.contains(&input_characters.next().unwrap()),
+        'w' => ALPHANUMERIC_CHARACTERS.contains(&input_characters.next().unwrap()),
+        '\\' => input_characters.next().unwrap() == '\\',
         _ => panic!("Unhandled escape pattern: {}", escape_pattern),
     }
 }
